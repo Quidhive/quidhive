845eb96bc13dd7f77469eb09013ec8d3a3a6705a
PeluMike <pmichael3006@gmail.com>
2025-10-20 03:08:43 -0600

    fix text inout zooming

--- src/components/card/Upcoming.tsx
+++ src/components/card/Upcoming.tsx
@@ -11,13 +11,15 @@ type UpcomingType = {
 
 function Upcoming({ img, title, text, textColor }: UpcomingType) {
   return (
-    <div className="bg-bg-50 px-10 sm:w-1/2 phone:w-[400px] m-auto flex flex-col items-center pb-6 rounded-t-[10px]">
-      <img src={img} className="w-[336px]" alt={title} />
+    <div className="bg-bg-50 phone:px-10 px-5 sm:w-1/2 phone:w-[400px] m-auto flex flex-col items-center pb-6 rounded-t-[10px]">
+      <img src={img} className="phone:w-[336px]" alt={title} />
       <div className="lg:w-[336px] m-auto mt-14">
         <div className="flex items-center gap-3">
-          <p className="font-semibold text-lg text-text-faded-3">{title}</p>
+          <p className="font-semibold phone:text-lg text-text-faded-3 phone:flex-none flex-1">
+            {title}
+          </p>
           <p
-            className={`text-xs bg-bg-25 px-2 py-[2px] rounded-full ${textColor}`}
+            className={`text-xs bg-bg-25 px-2 py-[2px] rounded-full w-[96px] ${textColor}`}
           >
             coming soon
           </p>
